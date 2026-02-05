893fbe17b1cb903cfaefa9c1473f31f0e87f136f
JHONATANGARCIA <jhonatansgomez.0311@gmail.com>
2026-02-05 15:14:46 -0500

    05/02/2026 3:14  PM

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -114,7 +114,7 @@ userCtrl.getUserById = async (req, res) => {
             jsonb_build_object(
             'municipality_id', u.municipality_id,
             'name', u.municipality,
-            'deparment', u.department) AS municipality,
+            'department', u.department) AS municipality,
 
             jsonb_build_object(
             'id', td.id,
