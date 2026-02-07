0793f0f3365ec0c23ef6deac0c3dd318acfc8203
Oscar Gomez <oscarr.2343@gmail.com>
2026-02-07 10:47:05 -0500

    Update middleware

--- middleware/webToken.middleware.js
+++ middleware/webToken.middleware.js
@@ -55,7 +55,7 @@ webToken.generateRefreshToken = (user) => {
 webToken.verifyJwt = (rolesAllowed = ["admin"]) => {
   return async (req, res, next) => {
     try {
-      const token = req.cookies.auth;
+      const token = req.headers["x-token"];
       if (!token) {
         return res.status(401).json({ msg: "No autenticado" });
       }
